26c228cfc33ee8312cd824d7306735970f2bf692
guistetter <guilherme_stetter@hotmail.com>
2020-06-11 13:25:39 -0300

    exibindo cardapio do banco de dados na pag index, com EJS EMBEDDED Javascript Templates

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
-var express = require('express');
+var connection = require("./../includes/db");
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  connection.query(
+    `
+    SELECT * FROM tb_menus ORDER BY title
+  `,
+    (err, results) => {
+      if (err) {
+        console.log(err);
+      }
+      res.render("index", {
+        title: "Restaurante Saboroso",
+        menus: results,
+      });
+    }
+  );
 });
 
 module.exports = router;
