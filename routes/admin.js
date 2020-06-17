a3bd36b6d6010b532ec9c02c7e72ba2fbb0b8126
guistetter <guilherme_stetter@hotmail.com>
2020-06-17 16:05:15 -0300

    fix bugs e deixando links do menu do administrativo dinamico, deixando menu ativo

--- routes/admin.js
+++ routes/admin.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var users = require("./../includes/users");
+var admin = require("./../includes/admin");
 var router = express.Router();
 
 //controlando area restrita do site
@@ -11,13 +12,22 @@ router.use(function (req, res, next) {
   }
   //console.log("middleware: ", req.url);
 });
+
+//middleware passar var menus do header para as rotas
+router.use(function (req, res, next) {
+  req.menus = admin.getMenus(req);
+  next();
+});
+
 router.get("/logout", function (req, res, next) {
   delete req.session.user;
   res.redirect("/admin/login");
 });
 
 router.get("/", function (req, res, next) {
-  res.render("admin/index");
+  res.render("admin/index", {
+    menus: req.menus,
+  });
 });
 
 router.post("/login", function (req, res, next) {
@@ -43,18 +53,26 @@ router.get("/login", function (req, res, next) {
 });
 
 router.get("/contacts", function (req, res, next) {
-  res.render("admin/contacts");
+  res.render("admin/contacts", {
+    menus: req.menus,
+  });
 });
 router.get("/emails", function (req, res, next) {
-  res.render("admin/emails");
+  res.render("admin/emails", {
+    menus: req.menus,
+  });
 });
 router.get("/menus", function (req, res, next) {
-  res.render("admin/menus");
+  res.render("admin/menus", {
+    menus: req.menus,
+  });
 });
 router.get("/reservations", function (req, res, next) {
-  res.render("admin/reservations", { date: {} });
+  res.render("admin/reservations", { menus: req.menus, date: {} });
 });
 router.get("/users", function (req, res, next) {
-  res.render("admin/users");
+  res.render("admin/users", {
+    menus: req.menus,
+  });
 });
 module.exports = router;
