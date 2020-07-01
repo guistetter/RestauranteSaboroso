13341d857ed883b21bb98410c67910363c032752
guistetter <guilherme_stetter@hotmail.com>
2020-07-01 12:30:40 -0300

    refatorando codigo, criando metodo para botal delete

--- public/js/hcode-grid.js
+++ public/js/hcode-grid.js
@@ -6,6 +6,21 @@ class HcodeGrid {
         //console.log('afterUpdateClick');
         $("#modal-update").modal('show');
       },
+      afterDeleteClick:(e) =>{
+        window.location.reload();
+      },
+      afterFromCreate: (e) => {
+        window.location.reload();
+      },
+      afterFormUpdate:(e)=>{
+        window.location.reload();
+      },
+      afterFormCreateError:(e)=>{
+        alert("nao foi possivel enviar o formulario");
+      },
+      afterFormUpdateError:(e)=>{
+        alert("nao foi possivel alterar o formulario");
+      }
     },configs.listeners);
 
     this.options = Object.assign({}, {
@@ -18,32 +33,44 @@ class HcodeGrid {
     this.initForms();
     this.initButtons();
 
-  }
+  }//fim constructor
 
   initForms(){
     this.formCreate = document.querySelector(this.options.formCreate)
 
     this.formCreate.save().then(json =>{
   
-      window.location.reload();
+      this.fireEvent("afterFromCreate");
   
     }).catch(err => {
+      this.fireEvent("afterFormCreateError");
         console.log(err)
     });
   
     this.formUpdate = document.querySelector(this.options.formUpdate);
   
     this.formUpdate.save().then(json =>{
-      window.location.reload();
+
+      this.fireEvent("afterFormUpdate");
+
     }).catch(err => {
+      this.fireEvent("afterFormUpdateError");
       console.log(err);
     });
   }//fim initForms
 
   fireEvent(name, args){
-
     if(typeof this.options.listeners[name] === 'function') 
       this.options.listeners[name].apply(this, args)
+  }
+
+  getTrData(e){
+    let tr = e.path.find(el => {
+      return (el.tagName.toUpperCase() === "TR");
+    })
+
+    return JSON.parse(tr.dataset.row)
+  
   }
 
   initButtons(){
@@ -52,29 +79,24 @@ class HcodeGrid {
     [...document.querySelectorAll(this.options.btnUpdate)].forEach(btn => {
   
       btn.addEventListener("click", e => {
-
         //this.options.listeners.beforeUpdateClick(e);
         this.fireEvent("beforeUpdateClick",[e])
-
-          let tr = e.path.find(el => {
-            return (el.tagName.toUpperCase() === "TR");
-          })
-  
-          let data = JSON.parse(tr.dataset.row)
-          console.log(data);
-  
+        let data = this.getTrData(e);
           for (let name in data){
             let input = this.formUpdate.querySelector(`[name=${name}]`);
+
             switch(name){
+
               case "date":
+
               if(input) input.value = moment(data[name]).format("YYYY-MM-DD");
                 console.log(data[name]);
                 console.log(moment(data[name]).format("YYYY-MM-DD"));
               break;
+
               default : 
               if(input) input.value = data[name];
             }
-    
           }
 
           this.fireEvent("afterUpdateClick",[e])
@@ -86,29 +108,26 @@ class HcodeGrid {
   
     [...document.querySelectorAll(this.options.btnDelete)].forEach(btn => {
       btn.addEventListener("click", e=>{
-    
-        let tr = e.path.find(el => {
-          return (el.tagName.toUpperCase() === "TR");
-        });
-
-        let data = JSON.parse(tr.dataset.row);
-
+        
+        this.fireEvent("beforeDeleteClick")
+        let data = this.getTrData(e);
+        
         if(confirm(eval("`" + this.options.deleteMsg + "`"))){
 
           console.log("data id delete " + data.id);
 
-        fetch(eval("`" + this.options.deleteUrl + "`"),{
+         fetch(eval("`" + this.options.deleteUrl + "`"),{
           method: "DELETE"
-        })
-        .then(response => response.json())
-        .then(json => {
+         })
+         .then(response => response.json())
+         .then(json => {
+            console.log(json)
+            this.fireEvent("afterDeleteClick");
+          });
+        }//fim if
 
-          console.log(json)
-          
-          window.location.reload();
-        });
-        } 
-      });
-    });
-  }
+      });//fim listener
+    });//fim delete
+  }//fim init buttons
+
 }
