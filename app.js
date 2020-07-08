a80f5fa95bed174b5c0244d569d34705db3e0aa7
guistetter <guilherme_stetter@hotmail.com>
2020-07-08 13:40:42 -0300

    instalando e configurando socket.io

--- app.js
+++ app.js
@@ -5,8 +5,12 @@ var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 var session = require("express-session");
 var RedisStore = require("connect-redis")(session);
-var formidable = require("formidable")
-var path = require("path")
+var formidable = require("formidable");
+
+var http = require('http');
+var socket = require('socket.io');
+
+var path = require("path");
 /**
  * var conectaRedis = require("connect-redis")
  * var RedisStore = connectRedis(session)
@@ -15,6 +19,13 @@ var indexRouter = require("./routes/index");
 var adminRouter = require("./routes/admin");
 
 var app = express();
+
+var http = http.Server(app);
+var io = socket(http);
+
+io.on("connection", function(socket){
+  console.log("Novo usuário conectado")
+});
 
 app.use(function(req,res, next){
   let contentType = req.headers["content-type"];
@@ -52,8 +63,8 @@ app.use(
 );
 
 app.use(logger("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+//app.use(express.json());
+//app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
@@ -76,4 +87,8 @@ app.use(function (err, req, res, next) {
   res.render("error");
 });
 
-module.exports = app;
+http.listen(3000, function(){
+  console.log("servidor em execucao ")
+})
+
+//module.exports = app; nao é necessario
